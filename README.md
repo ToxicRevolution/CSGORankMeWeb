34a86da4f0f92580e8dfb887504ce2831014b1ac
Andy Petrzilka <andypetrzilka@gmail.com>
2016-12-20 11:43:44 -0600

    Basic Layout + README changes
    Added basic structure with menus for index page.

--- README.md
+++ README.md
@@ -11,6 +11,22 @@ This web interface is to allow users to view the stats from Counter Strike Globa
 <li>Check stats of any player you desire that plays on the server(s) you are looking for.</li>
 </ul>
 
-#Installation
+# Ideads
+<ul>
+<li>Page for all database strings as functions so then when you need to get data from database you just call the function and use the returned data.  For instance, getTotalPlayrNumber() would select all player records from the player table and count them and return that number</li>
+ 
+<li>XML file for database connection info so that you can edit it later VIA either an admin page (probably) or editing the file.</li>
+ 
+<li>Searches for players by name or SteamID  (Automate with steamID converter to check various forms)</li>
+ 
+<li>Leaderboards that are sortable by any stat (Use querys above to get that info AJAX+Pagination)</li>
+ 
+<li>Live server stats (Map, players, players stats, connect button)</li>
+ 
+<li>mySQL only?</li>
+</ul>
+
+# Installation
 
 Coming Soon!
+
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+/*
+Fill in phpVariables with values from Database later.
+*/
 $webTitle = "test";
 $pageTitle = "test";
+$serverName = "DMFrenzy";
+$totalUsers = "6332";
 ?>
 <html>
 <head>
@@ -19,12 +24,23 @@ $pageTitle = "test";
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
+ <!-- Morris Charts CSS -->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
+ <!-- Custom Fonts -->
+ <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+
 
 <link rel="stylesheet" href="css/navCSS.css?ver=<?php echo date('l jS \of F Y h:i:s A'); ?>">
 
 </head>
 <body>
-<div class="container">
+<div class="container" style="width:95%;">
 	<div class="row">
 		<div class="wrapper">
     	    <div class="side-bar">
@@ -50,14 +66,41 @@ $pageTitle = "test";
                 </ul>
     	    </div>   
             <div class="content">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Panel heading without title</div>
-                        <div class="panel-body">
-                            Panel content
-                        </div>
-                    </div>
-                </div>
+            <br>
+            	<div class="row" style="padding-left: 5%;">
+            	    <div class="col-md-12">
+            	        <div class="panel panel-default">
+            	            <div class="panel-heading">Welcome to <?php echo $serverName;?></div>
+            	            	<div class="panel-body">
+                            With this page you can track your stats on any server running the RankMe plugin. You can also check where you fall on the leaderboards and who current sits atop the leaderboads. You can search and sort by various stats and check various graphs!
+            	            	</div>
+            	        </div>
+            	    </div>
+            	</div> <!-- ROW END -->
+            	<div class="row" style="padding-left: 5%;">
+            		<div class="col-md-3">
+            			<div class="panel panel-primary">
+                    	    <div class="panel-heading">
+                    	        <div class="row">
+                    	            <div class="col-xs-3">
+                    	                <i class="fa fa-user fa-5x"></i>
+                    	            </div>
+                    	            <div class="col-xs-9 text-right">
+                    	                <div class="huge"><b><?php echo $totalUsers; ?></b></div>
+                    	                <div><b>Total Users!</b></div>
+                    	            </div>
+                    	        </div>
+                    	    </div>
+                    	    <a href="#">
+                    	        <div class="panel-footer">
+                    	            <span class="pull-left">View LeaderBoards!</span>
+                    	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    	            <div class="clearfix"></div>
+                    	        </div>
+                    	    </a>
+                		</div>
+                	</div>
+            	</div> <!-- ROW END -->
             </div>
 		</div>
 	</div>
