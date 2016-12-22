65b5c9d2edafa2b2ed48aed62d4d684ebee692ad
Kenny Lindelof <kenny@kennyl.tv>
2016-12-22 15:12:15 -0500

    Added Views, Made Nav set active class based on $page

---
+++ views/home/index.php
@@ -0,0 +1,82 @@
+<div class="content">
+            <br>
+                <div class="row" style="padding-left: 5%;">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">Welcome to <?php echo $serverName;?></div>
+                                <div class="card-block">
+                            With this page you can track your stats on any server running the RankMe plugin. You can also check where you fall on the leaderboards and who current sits atop the leaderboads. You can search and sort by various stats and check various graphs!
+                                </div>
+                        </div>
+                    </div>
+                </div> <!-- ROW END -->
+                <div class="row" style="padding-left: 5%;">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-danger">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-search fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalSearch; ?></b></div>
+                                        <div><b>Total Searches</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">Search A User</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-primary">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalUsers; ?></b></div>
+                                        <div><b>Total Users!</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">View LeaderBoards!</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-success">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-server fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalServers; ?></b></div>
+                                        <div><b>Servers To Play On!</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">View LeaderBoards!</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- ROW END -->
+
+            </div>
