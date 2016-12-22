dd3bd0cc42c10e49e7dbc9b250023155c6b43cba
Kenny Lindelof <kenny@kennyl.tv>
2016-12-22 10:44:05 -0500

    Moved Nav to Nav View. Added infro to ReadME

--- views/nav/index.php
+++ views/nav/index.php
@@ -1,28 +1,23 @@
-<div class="side-bar">
-    <li class="menu-head">
-
-    </li>
-
-</div>
+        <div class="wrapper">
             <div class="side-bar">
                 <ul>
                     <li class="menu-head">
-                        <?php echo $pageTitle; ?> <a href="#" class="push_menu"><span class="glyphicon glyphicon-align-justify pull-right"></span></a>
+                        <?php echo $pageTitle; ?> <a href="#" class="push_menu"><span class="fa fa-reorder pull-right"></span></a>
                     </li>
                     <div class="menu">
                         <li>
-                            <a href="#" class="active">Home <span class="glyphicon glyphicon-home pull-right"></span></a>
+                            <a href="#" class="active">Home <span class="fa fa-home pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#"">Leaderboards <span class="glyphicon glyphicon-list-alt pull-right"></span></a>
+                            <a href="#"">Leaderboards <span class="fa fa-list-alt pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#">Player Search<span class="glyphicon glyphicon-search pull-right"></span></a>
+                            <a href="#">Player Search<span class="fa fa-search pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#">Servers <span class="glyphicon glyphicon-hdd pull-right"></span></a>
+                            <a href="#">Servers <span class="fa fa-server pull-right"></span></a>
                         </li>
                     </div>
                     
                 </ul>
-            </div>
+            </div>   
