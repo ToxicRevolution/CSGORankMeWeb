9697829d6fa4311884923ba940497fd697360a33
Kenny Lindelof <kenny@kennyl.tv>
2016-12-22 10:28:25 -0500

    Bootstrap 4, Fontawesome, Tether.io, as well as the start of less

--- index.php
+++ index.php
@@ -2,109 +2,148 @@
 /*
 Fill in phpVariables with values from Database later.
 */
-$webTitle = "test";
-$pageTitle = "test";
+$webTitle = "RankMe || DMFrenzy";
+$pageTitle = "DMFrenzy Servers";
 $serverName = "DMFrenzy";
 $totalUsers = "6332";
+$totalServers = "34";
+$totalSearch = "4403"
 ?>
 <html>
 <head>
-<title><?php echo $webTitle; ?></title>
-<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<!-- Latest compiled and minified Jquery JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <title>
+        <?php echo $webTitle; ?>
+    </title>
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Calling JS -->
+    <!-- Latest compiled and minified Jquery JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap 4 and Tether -->
+    <script src="js/tether/tether.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- Custom JS -->
+    <script src="js/Navjs.js"></script>
+    <!-- End Of JS -->
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
- <!-- Morris Charts CSS -->
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-
- <!-- Custom Fonts -->
- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-
-
-
-<link rel="stylesheet" href="css/navCSS.css?ver=<?php echo date('l jS \of F Y h:i:s A'); ?>">
-
+    <!-- Bootstrap 4, Tether, Fontawesome -->
+    <link rel="stylesheet" href="css/tether/tether.min.css">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/font-awesome.min.css">
+    <!-- Custom CSS --> 
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="container" style="width:95%;">
-	<div class="row">
-		<div class="wrapper">
-    	    <div class="side-bar">
+    <div class="container" style="width:95%;">
+    <div class="row">
+        <div class="wrapper">
+            <div class="side-bar">
                 <ul>
                     <li class="menu-head">
-                        <?php echo $pageTitle; ?> <a href="#" class="push_menu"><span class="glyphicon glyphicon-align-justify pull-right"></span></a>
+                        <?php echo $pageTitle; ?> <a href="#" class="push_menu"><span class="fa fa-reorder pull-right"></span></a>
                     </li>
                     <div class="menu">
                         <li>
-                            <a href="#" class="active">Home <span class="glyphicon glyphicon-dashboard pull-right"></span></a>
+                            <a href="#" class="active">Home <span class="fa fa-home pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#"">Leaderboards <span class="glyphicon glyphicon-heart pull-right"></span></a>
+                            <a href="#"">Leaderboards <span class="fa fa-list-alt pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#">Player Search<span class="glyphicon glyphicon-star pull-right"></span></a>
+                            <a href="#">Player Search<span class="fa fa-search pull-right"></span></a>
                         </li>
                         <li>
-                            <a href="#">Severs <span class="glyphicon glyphicon-cog pull-right"></span></a>
+                            <a href="#">Servers <span class="fa fa-server pull-right"></span></a>
                         </li>
                     </div>
                     
                 </ul>
-    	    </div>   
+            </div>   
             <div class="content">
             <br>
-            	<div class="row" style="padding-left: 5%;">
-            	    <div class="col-md-12">
-            	        <div class="panel panel-default">
-            	            <div class="panel-heading">Welcome to <?php echo $serverName;?></div>
-            	            	<div class="panel-body">
+                <div class="row" style="padding-left: 5%;">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">Welcome to <?php echo $serverName;?></div>
+                                <div class="card-block">
                             With this page you can track your stats on any server running the RankMe plugin. You can also check where you fall on the leaderboards and who current sits atop the leaderboads. You can search and sort by various stats and check various graphs!
-            	            	</div>
-            	        </div>
-            	    </div>
-            	</div> <!-- ROW END -->
-            	<div class="row" style="padding-left: 5%;">
-            		<div class="col-md-3">
-            			<div class="panel panel-primary">
-                    	    <div class="panel-heading">
-                    	        <div class="row">
-                    	            <div class="col-xs-3">
-                    	                <i class="fa fa-user fa-5x"></i>
-                    	            </div>
-                    	            <div class="col-xs-9 text-right">
-                    	                <div class="huge"><b><?php echo $totalUsers; ?></b></div>
-                    	                <div><b>Total Users!</b></div>
-                    	            </div>
-                    	        </div>
-                    	    </div>
-                    	    <a href="#">
-                    	        <div class="panel-footer">
-                    	            <span class="pull-left">View LeaderBoards!</span>
-                    	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    	            <div class="clearfix"></div>
-                    	        </div>
-                    	    </a>
-                		</div>
-                	</div>
-            	</div> <!-- ROW END -->
+                                </div>
+                        </div>
+                    </div>
+                </div> <!-- ROW END -->
+                <div class="row" style="padding-left: 5%;">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-danger">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-search fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalSearch; ?></b></div>
+                                        <div><b>Total Searches</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">Search A User</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-primary">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalUsers; ?></b></div>
+                                        <div><b>Total Users!</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">View LeaderBoards!</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header bg-success">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-server fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-xs-right">
+                                        <div class="huge"><b><?php echo $totalServers; ?></b></div>
+                                        <div><b>Servers To Play On!</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="card-footer">
+                                    <span class="pull-left">View LeaderBoards!</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- ROW END -->
+
             </div>
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
-<script src="js/Navjs.js"></script>
 </body>
 </html>
