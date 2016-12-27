2fd89778380d80f8bb9d60f365ead81ecaed5b41
Andy Petrzilka <andypetrzilka@gmail.com>
2016-12-27 08:50:30 -0600

    added create table in install for users
    InstallMethods will now create the table and an outline has been formed
    to now insert the new user into the database as well as create the file.

---
+++ install.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>Install/setup RankMe</title>
+
+		  <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+		  <script src="js/jquery-3.1.1.min.js"></script>
+		  <script src="js/tether/tether.min.js"></script>
+		  <script src="js/bootstrap/bootstrap.min.js"></script>
+
+
+
+		 <!-- Custom Fonts -->
+		 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="container">
+		  <div class="jumbotron">
+		    <h1>Installing/Setting up your web interface</h1> 
+		    <p>Please fill out the following information for setting up your site! All the information will be saved to a file named config.xml if you need to change it later!</p> 
+		  </div>
+		  <div class="panel panel-deafult">
+		  	<div class="panel-body">
+		  		<table>
+		  			<tr>
+		  				<td>Browser Title <a href="#" data-toggle="tooltip" title="This is the text that appears in the browser tabs will automatically added words like Home, Search or Leaderboards."><i class="fa fa-question" aria-hidden="true"></i> </a> &nbsp;&nbsp;</td>
+		  				<td><input type="text" name="browserTitle" id='installInputs' required/>
+		  		</table>
+		  	</div>
+		  </div>
+		  
+		</div>
+		<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script
+	</body>
+</html>
--- install/installMethods.php
+++ install/installMethods.php
@@ -1,12 +1,20 @@
 <?php 
-$fileMain = fopen("../config.php", w);
+$fileMain = fopen("../config.php", "w");
 $pageTitle = "$pageTitle = {$_POST['pageTitle']}\n";
 $webTitle = "$webTitle = {$_POST['browserTitle']}\n";
 $databaseConn = "$dbh = new PDO('mysql:host={$_POST['databaseIP']};dbname={$_POST['databaseName']}', '{$_POST['databaseUser']}', '{$_POST['databasePass']}');\n";
-
-$dbh = new PDO('mysql:host={$_POST['databaseIP']};dbname={$_POST['databaseName']}', '{$_POST['databaseUser']}', '{$_POST['databasePass']}');
-$createAdminDB = "";
+try{
+$dbh = new PDO("mysql:host={$_POST['databaseIP']};dbname={$_POST['databaseName']}", $_POST['databaseUser'], $_POST['databasePass']);
+$createAdminDB = "CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
 $stmt = $dbh->prepare($createAdminDB);
 $stmt->execute();
+} catch( PDOException $e ) {
+	throw new pdoDbException($e); 
+}
 
 ?>
