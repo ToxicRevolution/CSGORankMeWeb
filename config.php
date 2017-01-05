c06854ab274ff230ae9b37227854c7bc5d72a6f8
Andy Petrzilka <andypetrzilka@gmail.com>
2017-01-05 16:39:29 -0600

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("control/queryFunctions.php");
-require_once("control/class.Player.php");
-
-$pageTitle = 'DmFrenzy';
-$webTitle = 'DMFrenzy';
-$serverType = 'DM';
-$dbh = new PDO("mysql:host=localhost;dbname=petrzilk_rankMeTest", '', '');
-?>
