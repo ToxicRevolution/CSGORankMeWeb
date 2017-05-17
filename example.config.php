d04e9705d074c20c9b04083f048e0d3d3b6ae8c1
Kenny Lindelof <kennyLtv@users.noreply.github.com>
2017-05-17 15:11:23 -0400

    Adding example config
    Added example config for the users that seem to have issues with the install script

---
+++ example.config.php
@@ -0,0 +1,11 @@
+<?php
+require_once("control/queryFunctions.php");
+require_once("control/class.Player.php");
+require_once("control/class.Server.php");
+$pageTitle = '';
+$weaponsArray = array("m4a1_silencer","m4a1","ak47","awp","knife","usp_silencer","hkp2000","elite","p250","fiveseven","cz75a","deagle","glock","tec9","famas","aug","galilar","sg556","hegrenade","flashbang","smokegrenade","inferno");
+$webTitle = '';
+$dbh = new PDO("mysql:host=examplehost ;dbname=exampledb ", 'db_user', 'db_pass ');
+$webURL = ' ';
+$sbURL = ' ';
+?>
