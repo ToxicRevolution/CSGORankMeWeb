d6ffd829547e2b0785488191df3822a3ea8b1e45
Kenny Lindelof <kennyLtv@users.noreply.github.com>
2018-03-28 14:23:04 -0400

    updated to include new variables

--- example.config.php
+++ example.config.php
@@ -5,8 +5,10 @@ require_once("control/class.Server.php");
 $pageTitle = '';
 $weaponsArray = array("m4a1_silencer","m4a1","ak47","awp","knife","usp_silencer","hkp2000","elite","p250","fiveseven","cz75a","deagle","glock","tec9","famas","aug","galilar","sg556","hegrenade","flashbang","smokegrenade","inferno");
 $webTitle = '';
+$serverType = '';
+$table = 'rankme';
 $dbh = new PDO("mysql:host=examplehost ;dbname=exampledb ", 'db_user', 'db_pass ');
 $webURL = ' ';
 $sbURL = ' ';
-$title = "rankme";
+
 ?>
