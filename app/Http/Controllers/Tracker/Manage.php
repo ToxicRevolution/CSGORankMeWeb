72644107296206f95978748cc9bbabb83a719dee
Kenny Lindelof <lindelofkenny@gmail.com>
2018-06-30 18:46:59 -0400

    working on vac checker and Auth not working as I am pushing for trip

---
+++ app/Http/Controllers/Tracker/Manage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Tracker;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\VacLink;
+
+class Manage extends Controller
+{
+    public function delete(Request $request){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 // basic statistics about your players
 Route::get("/panelstats/{con?}", "RankMeData@panelStats");
-
 // Routing for parameterized paginated database responses
 Route::get("/all/{con?}", "RankMeData@getPaginated");
-
 // Get player profiles
 Route::get("/player/{id}/{con?}", "RankMeData@getProfile");
-
 // Get top X players
 Route::get("/top/{count}/{con?}", "RankMeData@getTopPlayers");
+// Delete a VAC'd Player
+Route::post("/vac/add", "Tracker\Manage@delete")->middleware("auth:api");
 
