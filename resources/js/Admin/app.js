0aa081f9fd56aec40ad51bb59c32c0bb25e7d8e7
Kenny Lindelof <lindelofkenny@gmail.com>
2018-10-23 11:58:57 -0400

    changed to react from vue

---
+++ resources/js/Admin/app.js
@@ -0,0 +1,8 @@
+require('../bootstrap');
+import React from "react";
+import ReactDom from "react-dom";
+import Nav from './Nav';
+ReactDom.render(
+    <Nav/>,
+    document.getElementById("AdminApp")
+)
---
+++ resources/js/Admin/components/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Axios from 'axios';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className='w-full mr-2 ml-2 flex flex-col md:flex-row text-white'>
+                Home
+            </div>
+        )
+    }
+}
+export default Home;
